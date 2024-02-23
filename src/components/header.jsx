3c51a904522cbc3e9205bebd76963180a5ab86c8
Claudia M Cardona H <80611384+claumcarh@users.noreply.github.com>
2024-02-23 08:14:39 -0500

    Se agrega adiciona lista de navegacion del Header principal
    Tarea: 1273

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import giphyLogo from "../assets/images/giphy-logo.svg";
 import giphyText from "../assets/images/giphy-text.svg";
 import giphyPlus from "../assets/images/giphy-plus.svg";
@@ -6,6 +7,7 @@ import giphyHamburgerMenu from "../assets/images/giphy-hamburger-menu.svg";
 import "./header.css";
 
 function Header() {
+  const [navOpened, setNavOpened] = useState(false);
   return (
     <header className="header">
       <a className="header__logo" href="/">
@@ -17,19 +19,179 @@ function Header() {
         <img className="header__plus-img" src={giphyPlus} alt="" />
       </a>
 
-      <button className="header__user"
-      href="/"
-      >
+      <button className="header__user">
         <img className="header__user-img" src={giphyPersonIcon} alt="" />
       </button>
 
-      <button className="header__hamburgerMenu" href="/">
-        <img
-          className="header__hamburgerMenu-img"
-          src={giphyHamburgerMenu}
-          alt=""
-        />
-      </button>
+      <nav className="header__nav">
+        <button
+          className="header__hamburgerMenu"
+          onClick={() => setNavOpened((opened) => !opened)}
+        >
+          <img
+            className="header__hamburgerMenu-img"
+            src={giphyHamburgerMenu}
+            alt=""
+          />
+        </button>
+        <div className={`nav ${navOpened ? 'nav--shown' : ''}`}>
+          <ul className="nav__list">
+            <li className="nav__item nav__item--categories">
+              <a className="nav__sublist-title" href="/">
+                Categories
+              </a>
+              <ul className="nav__list nav__sublist">
+                <li>
+                  <a className="nav__sublist-item" href="/">
+                    GIPHY Studios
+                  </a>
+                </li>
+                <li>
+                  <a className="nav__sublist-item" href="/">
+                    Animals
+                  </a>
+                </li>
+                <li>
+                  <a className="nav__sublist-item" href="/">
+                    Artists
+                  </a>
+                </li>
+                <li>
+                  <a className="nav__sublist-item" href="/">
+                    Emotions
+                  </a>
+                </li>
+                <li>
+                  <a className="nav__sublist-item" href="/">
+                    Entertainment
+                  </a>
+                </li>
+                <li>
+                  <a className="nav__sublist-item" href="/">
+                    Gaming
+                  </a>
+                </li>
+                <li>
+                  <a className="nav__sublist-item" href="/">
+                    Holidays/Greetings
+                  </a>
+                </li>
+                <li>
+                  <a className="nav__sublist-item" href="/">
+                    Reactions
+                  </a>
+                </li>
+                <li>
+                  <a className="nav__sublist-item" href="/">
+                    Sports
+                  </a>
+                </li>
+                <li>
+                  <a className="nav__sublist-item" href="/">
+                    Clips
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav__item nav__item--stickers">
+              <a className="nav__sublist-title" href="/">
+                Stickers
+              </a>
+              <ul className="nav__list nav__sublist">
+                <li>
+                  <a className="nav__sublist-item" href="/">
+                    Originals
+                  </a>
+                </li>
+                <li>
+                  <a className="nav__sublist-item" href="/">
+                    Trending
+                  </a>
+                </li>
+                <li>
+                  <a className="nav__sublist-item" href="/">
+                    Reactions
+                  </a>
+                </li>
+                <li>
+                  <a className="nav__sublist-item" href="/">
+                    Packs
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav__item nav__item--apps">
+              <a className="nav__sublist-title" href="/">
+                Apps
+              </a>
+              <ul className="nav__list nav__sublist">
+                <li>
+                  <a className="nav__sublist-item" href="/">
+                    GIPHY
+                  </a>
+                </li>
+                <li>
+                  <a className="nav__sublist-item" href="/">
+                    GIPHY World
+                  </a>
+                </li>
+                <li>
+                  <a className="nav__sublist-item" href="/">
+                    GIPHY Keys
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav__item nav__item--about">
+              <a className="nav__sublist-title" href="/">
+                About
+              </a>
+              <ul className="nav__list nav__sublist">
+                <li>
+                  <a className="nav__sublist-item" href="/">
+                    DMCA
+                  </a>
+                </li>
+                <li>
+                  <a className="nav__sublist-item" href="/">
+                    FAQ
+                  </a>
+                </li>
+                <li>
+                  <a className="nav__sublist-item" href="/">
+                    Jobs
+                  </a>
+                </li>
+                <li>
+                  <a className="nav__sublist-item" href="/">
+                    Engineering Blog
+                  </a>
+                </li>
+                <li>
+                  <a className="nav__sublist-item" href="/">
+                    GIPHY Arts
+                  </a>
+                </li>
+                <li>
+                  <a className="nav__sublist-item" href="/">
+                    Community
+                  </a>
+                </li>
+                <li>
+                  <a className="nav__sublist-item" href="/">
+                    Guidelines
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <footer className="footer">
+            <p className="copyright">&copy; 2024 GIPHY, Inc.</p>
+            <p className="">Terms of Service Community Guidelines Privacy Policy</p>
+            <p className="">Copyright Manage Cookies</p>
+          </footer>
+        </div>
+      </nav>
     </header>
   );
 }
